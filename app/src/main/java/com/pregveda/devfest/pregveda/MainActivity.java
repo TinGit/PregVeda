9cf26b371c0279235b6ad3083013caa7ab3c578e
TinGit <tinbit1@gmail.com>
2015-12-26 16:01:56 -0600

    first commit

--- app/src/main/java/com/pregveda/devfest/pregveda/MainActivity.java
+++ app/src/main/java/com/pregveda/devfest/pregveda/MainActivity.java
@@ -1,31 +1,41 @@
 package com.pregveda.devfest.pregveda;
 
 import android.os.Bundle;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.profile);
+     //   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+     //   imageButton = (ImageButton) findViewById(R.id.imageButton);
+     //   setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+       /* imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
     }
 
     @Override
