c611717acc50e542b54c713c166d08a191532a16
TinGit <tinbit1@gmail.com>
2015-12-28 01:22:35 -0600

    Merge branch 'master' of https://github.com/TinGit/PregVeda
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml

--- app/src/main/java/com/pregveda/devfest/pregveda/meditation.java
+++ app/src/main/java/com/pregveda/devfest/pregveda/meditation.java
@@ -2,15 +2,30 @@ package com.pregveda.devfest.pregveda;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.ImageButton;
+import android.view.View;
 /**
  * Created by user on 12/27/15.
  */
 public class meditation extends Activity {
 
+    ImageButton imgBtn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meditation);
+
+
+        imgBtn = (ImageButton) findViewById(R.id.medhome_btn);
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
     }
+
+
 }
