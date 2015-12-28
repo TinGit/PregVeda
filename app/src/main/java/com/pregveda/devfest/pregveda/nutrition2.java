5c8b8e6bd1b0fd261b8f6b4b72f91463277bf582
Friat Mehari <Fri4Adom@gmail.com>
2015-12-28 02:14:36 -0600

    commit

--- app/src/main/java/com/pregveda/devfest/pregveda/nutrition2.java
+++ app/src/main/java/com/pregveda/devfest/pregveda/nutrition2.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Fri4A on 12/27/2015.
@@ -12,19 +15,22 @@ import android.widget.Button;
 
 public class nutrition2 extends Activity {
 
+    TextView textn1;
     /**
      * Called when the activity is first created.
      */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nutrition2);
-
+        textn1 = (TextView) findViewById(R.id.textViewn1);
         Button next = (Button) findViewById(R.id.button2);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent();
+               /* Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
+                */
+                textn1.setText("Hello");
             }
 
         });
