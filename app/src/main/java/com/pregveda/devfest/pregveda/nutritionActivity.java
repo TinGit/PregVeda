a57143fa55d764245fc1badb57cb3a32ed3225f2
eepphhrreemm <eepphhrreemm@gmail.com>
2015-12-28 15:46:23 -0600

    newLifeStyle

--- app/src/main/java/com/pregveda/devfest/pregveda/nutritionActivity.java
+++ app/src/main/java/com/pregveda/devfest/pregveda/nutritionActivity.java
@@ -7,25 +7,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class nutritionActivity extends AppCompatActivity {
 
+    ImageButton imgbtnHome;
     TextView textViewnu4;
-    Button buttonNext;
+    ImageButton imgbtnNext;
     ImageView ml;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition);
-//        buttonNext=(Button)findViewById(R.id.buttonNext);
+       imgbtnNext=(ImageButton)findViewById(R.id.imgbtnNext);
         textViewnu4=(TextView)findViewById(R.id.textViewnu4);
-        buttonNext.setOnClickListener(new View.OnClickListener() {
+        imgbtnHome=(ImageButton)findViewById(R.id.imgbtnHome);
+        imgbtnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textViewnu4.setText("I change this text now so here it is ");
 
+            }
+        });
+        imgbtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 
