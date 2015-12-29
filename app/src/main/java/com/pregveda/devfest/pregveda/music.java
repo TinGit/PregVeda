9ed0aaa4e83c3877a6662f73a81bf2abbf51664d
eepphhrreemm <eepphhrreemm@gmail.com>
2015-12-28 23:30:08 -0600

    Music Update

--- app/src/main/java/com/pregveda/devfest/pregveda/music.java
+++ app/src/main/java/com/pregveda/devfest/pregveda/music.java
@@ -1,16 +1,13 @@
 package com.pregveda.devfest.pregveda;
 
-/**
- * Created by Mike on 12/26/2015.
- */
-
 import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.os.Handler;
@@ -21,11 +18,8 @@ public class music extends AppCompatActivity {
     private TextView songName, songDuration;
     private SeekBar seekBar;
     private double timeStart = 0, finalTime = 0;
-
-
-
-    //play button
-    private ImageButton btnPlay;
+    private int forwardTime = 2000, backwardTime = 2000;
+    private Handler durationHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,28 +27,74 @@ public class music extends AppCompatActivity {
         setContentView(R.layout.musiclay);
         songName = (TextView) findViewById(R.id.songName);
         songDuration = (TextView) findViewById(R.id.songDuration);
-        mediaPlayer = MediaPlayer.create(this, R.raw.garbhsanskar);
+        mediaPlayer = MediaPlayer.create(this, R.raw.veena);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
-        songName.setText("garbhsanskar.mp3");
-        seekBar.setMax((int) finalTime);
-        seekBar.setClickable(false);
-
-        //play button
-        btnPlay=(ImageButton)findViewById(R.id.btn_Play);
-        btnPlay.setOnClickListener(new View.OnClickListener() {
+        songName.setText("Veena.mp3");
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setClickable(true);
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                play(v);
+            public boolean onTouch(View v, MotionEvent event) {
+                if (mediaPlayer.isPlaying()) {
+                    SeekBar sb = (SeekBar) v;
+                    mediaPlayer.seekTo(sb.getProgress());
+                }
+                return false;
             }
         });
     }
-
-
+    private Runnable updateSeekBarTime = new Runnable() {
+        public void run() {
+            timeStart = mediaPlayer.getCurrentPosition();
+            seekBar.setProgress((int) timeStart);
+            double timeRemaining = finalTime - timeStart;
+            songDuration.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining),
+                    TimeUnit.MILLISECONDS.toSeconds((long) timeRemaining) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                            timeRemaining))));
+            durationHandler.postDelayed(this, 100);
+        }
+    };
     public void play(View view) {
         mediaPlayer.start();
         timeStart = mediaPlayer.getCurrentPosition();
         seekBar.setProgress((int) timeStart);
-
+        durationHandler.postDelayed(updateSeekBarTime, 100);
     }
 
+    /* @Override
+     protected void onPause() {
+         super.onPause();
+         mediaPlayer.release();
+     }
+ */
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+    public void forward(View view) {
+        if ((timeStart + forwardTime) <= finalTime) {
+            timeStart = timeStart - backwardTime;
+            mediaPlayer.seekTo((int) timeStart);
+        }
+    }
+    public void backforward(View view) {
+        //check if we can go back at backwardTime seconds after song starts
+        if ((timeStart - backwardTime) > 0) {
+            timeStart = timeStart - backwardTime;
+            mediaPlayer.seekTo((int) timeStart);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
