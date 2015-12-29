bd689dae2ddc2f2b3da34fcdb7dd37cd2018aea0
eepphhrreemm <eepphhrreemm@gmail.com>
2015-12-28 20:28:22 -0600

    Profile_1

--- app/src/main/java/com/pregveda/devfest/pregveda/ProfileActivity.java
+++ app/src/main/java/com/pregveda/devfest/pregveda/ProfileActivity.java
@@ -6,13 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    Button btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btnSave=(Button)findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
